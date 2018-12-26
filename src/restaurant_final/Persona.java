8199a9c133650cb1d920d83d5c426f81c758cddc
jamilmendez <46139789+jamilmendez@users.noreply.github.com>
2018-12-25 19:24:38 -0500

    Update Persona.java

--- src/restaurant_final/Persona.java
+++ src/restaurant_final/Persona.java
@@ -1,4 +1,47 @@
 package restaurant_final;
 
 public class Persona {
+  
+    private String nombre;
+    private String nacionalidad;
+    private String direccion;
+    private String dni;
+
+    public Persona(String nombre, String nacionalidad, String dni) {
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
 }
